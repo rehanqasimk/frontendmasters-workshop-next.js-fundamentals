f0b6fef8b827007574e8ee822c5e2a7889f69319
rehanqasimk <qkrehan@gmail.com>
2025-05-01 18:56:51 +0500

    progress

---
+++ app/dashboard/new/page.tsx
@@ -0,0 +1,9 @@
+const NewIssuePage = () => {
+  return (
+    <div>
+      <h1>New Issue</h1>
+      <p>This is the new issue page.</p>
+    </div>
+  )
+}
+export default NewIssuePage
---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+const EditIssuePage = () => {
+  return (
+    <div>
+      <h1>Edit Issue</h1>
+      <p>This is the edit issue page.</p>
+    </div>
+  )
+}
+export default EditIssuePage
